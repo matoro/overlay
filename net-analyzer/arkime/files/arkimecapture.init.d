9d92370d515ba7e5235a674b7ebcb24d23da6aa5
matoro <matoro@users.noreply.github.com>
2023-02-04 17:54:11 -0500

    net-analyzer/arkime: add dependency on network interface for capture

--- net-analyzer/arkime/files/arkimecapture.init.d
+++ net-analyzer/arkime/files/arkimecapture.init.d
@@ -2,9 +2,15 @@
 # Copyright 1999-2022 Gentoo Authors
 # Distributed under the terms of the GNU General Public License v2
 
+function nodename() {
+    [[ "${RC_SVCNAME#*.}" == "arkimecapture" ]] && echo "default" && return
+    echo "${RC_SVCNAME#*.}"
+}
+
 function nodeargs() {
-    [[ "${RC_SVCNAME#*.}" == "arkimecapture" ]] && return
-    echo "-n ${RC_SVCNAME#*.}"
+    local name="$(nodename)"
+    [[ "${name}" == "default" ]] && return
+    echo "-n ${name}"
 }
 
 description="Arkime Capture"
@@ -13,6 +19,11 @@ command_args="-c \"${EPREFIX}/usr/lib/arkime/etc/config.ini\" $(nodeargs) ${OPTI
 pidfile="${EROOT}/run/${RC_SVCNAME}.pid"
 command_background=yes
 directory="${EPREFIX}/usr/lib/arkime"
+
+depend() {
+    local interface="$(grep -Pzo "\[$(nodename)\](.*\n)*" /etc/arkime/config.ini | grep -Paio "(?<=^interface=).+" | head -n 1)"
+    need "net.${interface}"
+}
 
 start_pre() {
     ebegin "Preparing interfaces for sniffing"
